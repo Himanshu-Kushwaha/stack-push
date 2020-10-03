48bdcf0c7def715d77aa0e98deb6f2a2105f071e
Himanshu-Kushwaha <himanshukushwaha@outlook.com>
2020-10-03 23:26:18 +0530

    Update psqlConfig.java

--- src/main/java/com/stack/operation/stackpush/config/psqlConfig.java
+++ src/main/java/com/stack/operation/stackpush/config/psqlConfig.java
@@ -35,7 +35,7 @@ public class psqlConfig {
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder,
 			@Qualifier("dataSource") DataSource dataSource) {
 		HashMap<String, Object> properties = new HashMap<>();
-		properties.put("hibernate.hbm2ddl.auto", "create");
+		properties.put("hibernate.hbm2ddl.auto", "update");
 		properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 		return builder.dataSource(dataSource).properties(properties)
 				.packages("com.stack.operation.stackpush.entity").persistenceUnit("StackData").build();
