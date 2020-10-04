cf3e6e3a263a4a4f0b6665e75763b9a522b08e02
Himanshu-Kushwaha <himanshukushwaha@outlook.com>
2020-10-04 11:35:27 +0530

    Update StackPushApplicationTest.java

--- src/test/java/com/stack/operation/stackpush/StackPushApplicationTest.java
+++ src/test/java/com/stack/operation/stackpush/StackPushApplicationTest.java
@@ -1,5 +1,22 @@
 package com.stack.operation.stackpush;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class StackPushApplicationTest {
 
+    @LocalServerPort
+    private int port;
+    @Test
+    public void contextLoads() {
+    }
+    
 }
